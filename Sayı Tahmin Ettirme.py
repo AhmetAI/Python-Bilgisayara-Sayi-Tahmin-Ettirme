8713ef49cb00e432edebaa6277b64b0968c908b7
Ahmet Arda İpek <49074377+ahmetardaipek@users.noreply.github.com>
2022-02-20 20:42:13 +0300

    Update Sayı Tahmin Ettirme.py

--- Sayı Tahmin Ettirme.py
+++ Sayı Tahmin Ettirme.py
@@ -1,28 +1,51 @@
 import random
+import threading
+import time
 
-karaliste = []
+zaman = 0
 
-min_sayi = int(input("Üretilecek Minimum Sayı - "))
-max_sayi = int(input("Üretilecek Maximum Sayı - "))
-bulunacak_sayi = int(input("Bulunacak Sayı -  "))
+def sure():
+    global zaman
+    while (zaman < 1000000):
+        zaman = zaman + 1
+        time.sleep(1)
+    return zaman
 
-if (bulunacak_sayi > max_sayi) or (bulunacak_sayi < min_sayi):
-    print("Bulunacak Sayı Maximum/Minimum Sayıdan Fazla/Az Olamaz")
-    exit()
 
-sayı = random.randint(min_sayi, max_sayi)
-if (sayı != bulunacak_sayi):
-    karaliste.append(sayı)
+def komut():
 
-while (sayı != bulunacak_sayi):
+    karaliste = []
+
+    min_sayi = int(input("Üretilecek Minimum Sayı - "))
+    max_sayi = int(input("Üretilecek Maximum Sayı - "))
+    bulunacak_sayi = int(input("Bulunacak Sayı -  "))
+
+    if (bulunacak_sayi > max_sayi) or (bulunacak_sayi < min_sayi):
+        print("Bulunacak Sayı Maximum/Minimum Sayıdan Fazla/Az Olamaz")
+        exit()
+
+    surethreading.start()
+    print("Aranıyor...")
     sayı = random.randint(min_sayi, max_sayi)
-    if (sayı not in karaliste):
+    if (sayı != bulunacak_sayi):
         karaliste.append(sayı)
 
+    while (sayı != bulunacak_sayi):
+        sayı = random.randint(min_sayi, max_sayi)
+        if (sayı not in karaliste):
+            karaliste.append(sayı)
 
-print("Denelinen Sayılar :")
-print(karaliste)
-print("Sayı Bulundu")
-print("Minimum Sayı", min_sayi, "\nMaximum Sayı",max_sayi )
-print("Aranılan Sayı ",bulunacak_sayi)
-print(len(karaliste),". Denemede Bulundu")
+    print("Denelinen Sayılar :")
+    print(karaliste)
+    print("Sayı Bulundu")
+    print("Minimum Sayı", min_sayi, "\nMaximum Sayı", max_sayi)
+    print("Aranılan Sayı ", bulunacak_sayi)
+    print(len(karaliste), ". Denemede ",zaman,"Saniyede Bulundu.")
+    exit()
+
+
+
+surethreading = threading.Thread(target=sure)
+komutthreading = threading.Thread(target=komut)
+
+komutthreading.start()
